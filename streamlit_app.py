69a347a16d0d7a2572743b779c9186829bdbc1de
Surya Utharakumar <111289565+suryautharakumar@users.noreply.github.com>
2025-11-27 19:42:00 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ st.markdown("""
         .stSelectbox label, .stNumberInput label {
             font-weight: 800;
             font-weight: bold;
-            color: #06d4fc;
+            color: #79d125;
         }
         [data-testid="stSidebar"] {
             background-image: url("https://raw.githubusercontent.com/suryautharakumar/CMP7005_Programming_for_Data_Analysis/refs/heads/main/dl.beatsnoop.com-3000-xPqy47jcDi.jpg");
