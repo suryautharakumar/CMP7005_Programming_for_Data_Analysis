d463c816fed9ac298eb958be6251418fbb9f8005
Surya Utharakumar <111289565+suryautharakumar@users.noreply.github.com>
2025-11-27 17:23:23 +0000

    Change image border radius to 50%

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,7 @@ st.markdown("""
 st.sidebar.markdown(
     """
     <div style="display: flex; align-items: center; margin-bottom: 20px;">
-        <img src="https://www.cardiffmet.ac.uk/media/cardiff-met/site-assets/images/apple-touch-icon.png" width="50" style="margin-right: 15px; border-radius:5rem;">
+        <img src="https://www.cardiffmet.ac.uk/media/cardiff-met/site-assets/images/apple-touch-icon.png" width="50" style="margin-right: 15px; border-radius:50%;">
         <h1 style="font-size: 1.5em; margin: 0; color:#ffffff">Navigation Menu</h1>
     </div>
     """,
